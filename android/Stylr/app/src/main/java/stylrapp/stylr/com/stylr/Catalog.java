a47312cc23122b690f337b8bf8c790e377735ee8
cowchimp <cowchimp@users.noreply.github.com>
2015-07-15 20:26:58 +0300

    adding the android app!

---
+++ android/Stylr/app/src/main/java/stylrapp/stylr/com/stylr/Catalog.java
@@ -0,0 +1,7 @@
+package stylrapp.stylr.com.stylr;
+
+public class Catalog {
+    Integer id;
+    String name;
+    String image;
+}
---
+++ android/Stylr/app/src/main/java/stylrapp/stylr/com/stylr/MainActivity.java
@@ -0,0 +1,73 @@
+package stylrapp.stylr.com.stylr;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+import java.util.List;
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final MainActivity mainActivity = this;
+        fetchCatalogs(new Callback<List<Catalog>>() {
+            @Override
+            public void success(List<Catalog> catalogs, Response response) {
+                Catalog catalog = catalogs.get(0);
+                setCatalogImage(catalog, mainActivity);
+                setCatalogItems(catalog);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+    }
+
+    private void setCatalogImage(Catalog catalog, MainActivity context) {
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Picasso.with(context).load(catalog.image).into(imageView);
+    }
+
+    private void setCatalogItems(Catalog catalog) {
+    }
+
+    private void fetchCatalogs(Callback<List<Catalog>> callback) {
+        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint("http://json.cowchimp.com").build();
+        CatalogService service = restAdapter.create(CatalogService.class);
+        service.listCatalogs(callback);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
